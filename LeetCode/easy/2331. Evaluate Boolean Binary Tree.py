00b310f069c5d9aa144637d3e24fdafb26a2329e
hauntedcupoftea <andydchauhan@gmail.com>
2024-05-16 20:21:56 +0530

    fix daily 16 may

--- LeetCode/easy/2331. Evaluate Boolean Binary Tree.py
+++ LeetCode/easy/2331. Evaluate Boolean Binary Tree.py
@@ -1,5 +1,9 @@
 from typing import Optional
-
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
         if not (root.left or root.right):
